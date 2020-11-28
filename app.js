033dc04830a313de20443c3eed90e4b2d4047be7
Mingjie Jiang <jiang@mingjie.dev>
2020-11-27 20:02:50 -0500

    minify

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const config = require("./config");
 const express = require("express");
 const path = require("path");
 const AirtablePlus = require("airtable-plus");
-const cookieParser = require('cookie-parser');
+const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
+const minifyHTML = require('express-minify-html')
+const compression = require('compression')
 
 const airtable = new AirtablePlus({
   baseID: config.airtable.base,
@@ -15,61 +17,78 @@ const airtable = new AirtablePlus({
 const app = express();
 const http = require("http").createServer(app);
 
-app.engine('html', require('ejs').renderFile); 
+app.engine("html", require("ejs").renderFile);
 app.use(
-    bodyParser.urlencoded({
+  bodyParser.urlencoded({
     extended: true,
   })
 );
 app.use(cookieParser());
+app.use(
+  minifyHTML({
+    override: true,
+    exception_url: false,
+    htmlMinifier: {
+      removeComments: true,
+      collapseWhitespace: true,
+      collapseBooleanAttributes: true,
+      removeAttributeQuotes: true,
+      removeEmptyAttributes: true,
+      minifyJS: true,
+      minifyCSS: true,
+    },
+  })
+);
+app.use(compression());
 
 const guessedMiddleware = (req, res, next) => {
-    if (req.cookies && req.cookies.attempted) {
-        res.redirect('/result')
-    } else {
-        next()
-    }
-}
+  if (req.cookies && req.cookies.attempted) {
+    res.redirect("/result");
+  } else {
+    next();
+  }
+};
 
-app.get('/', guessedMiddleware, (req, res) => {
-    res.render(__dirname + "/views/index.html")
-})
+app.get("/", guessedMiddleware, (req, res) => {
+  res.render(__dirname + "/views/index.html");
+});
 
 app.get("/result", async (req, res) => {
-    const allData = await airtable.read()
+  const allData = await airtable.read();
 
-    let n = 0
-    let x = 0
-    let a = [["Guess"]]
+  let n = 0;
+  let x = 0;
+  let a = [["Guess"]];
 
-    allData.forEach((d) => {
-        a.push([d.fields.guess])
-        if (d.fields.guess <= 50)
-            x++
-        n++
-    })
+  allData.forEach((d) => {
+    a.push([d.fields.guess]);
+    if (d.fields.guess <= 50) x++;
+    n++;
+  });
 
-    res.render(__dirname + "/views/results.html", {
-        p: (x / n * 100).toFixed(2),
-        n,
-        a: JSON.stringify(a)
-    })
-})
+  res.render(__dirname + "/views/results.html", {
+    p: ((x / n) * 100).toFixed(2),
+    n,
+    a: JSON.stringify(a),
+  });
+});
 
 app.post("/submit", guessedMiddleware, async (req, res) => {
   const guess = req.body.value;
   const email = req.body.email;
 
-  await airtable.create({
+  await airtable
+    .create({
       guess: Math.round(guess),
       email: email,
-      ts: new Date().toISOString()
-  }).catch((err) => {
-      console.log(err)
-  })
+      ts: new Date().toISOString(),
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 
-  res.cookie('attempted', true, { maxAge: 253402300000000 });
-  res.redirect('/result')
+  res.cookie("attempted", true, { maxAge: 253402300000000 });
+  res.redirect("/result");
 });
 
 app.use("/static", express.static(path.join(__dirname, "public")));
